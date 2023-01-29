e64ffe629b0513681b72c7b7b6870594979a68ad
Terri Mack <terrimack15@gmail.com>
2023-01-28 21:02:20 -0800

    edited html file to a simplified form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
+
+ // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
     // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -20,4 +20,8 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
+
+// current time in jumbotron
+$('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
+
+  
